2f39f19e950063ddd2cf56ec0ac6df43c3964a19
Ray Zhu <r2qokk@users.noreply.github.com>
2020-08-18 04:45:00 +0000

    Add path name to pk two way conversion

--- assignment/shorten_url/models.py
+++ assignment/shorten_url/models.py
@@ -1,3 +1,31 @@
+from math import pow
+from string import ascii_uppercase
+
 from django.db import models
 
-# Create your models here.
+char_to_int = {char: int_ for int_, char in enumerate(ascii_uppercase)}
+int_to_char = {int_: char for int_, char in enumerate(ascii_uppercase)}
+ranks = tuple(pow(26, i) for i in range(5))
+
+
+def from_char_to_pk(chars):
+    if len(chars) != 5:
+        raise ValueError
+    result = [char_to_int[char] * ranks[rank]
+              for rank, char in enumerate(chars[::-1])]
+    return int(sum(result))
+
+
+def from_pk_to_char(pk):
+    result = ''
+    for rank in reversed(ranks):
+        result += int_to_char[pk // rank]
+        pk %= rank
+    return result
+
+
+class Record(models.Model):
+    URL = models.URLField(unique=True)
+
+    def to_chars(self):
+        return from_pk_to_char(self.pk)
--- assignment/shorten_url/tests.py
+++ assignment/shorten_url/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from .models import from_char_to_pk, from_pk_to_char
+
+
+def test_char_to_int():
+    assert from_char_to_pk('AAAAB') == 1
+    assert from_char_to_pk('AAABB') == 27
+    assert from_char_to_pk('ZZZZZ') == 11881375
+
+    with pytest.raises(ValueError):
+        from_char_to_pk('ASDW')
+    with pytest.raises(KeyError):
+        from_char_to_pk('aaaab')
+    with pytest.raises(TypeError):
+        from_char_to_pk(123)
+
+
+def test_int_to_char():
+    assert from_pk_to_char(1) == 'AAAAB'
+    assert from_pk_to_char(27) == 'AAABB'
+    assert from_pk_to_char(10 ** 7 + 1) == 'VWYXL'
+
+    with pytest.raises(KeyError):
+        from_pk_to_char(10 ** 8)
+    with pytest.raises(TypeError):
+        from_pk_to_char('AAAAA')
